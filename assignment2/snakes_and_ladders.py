90ff4f6bd3e5eb6dbd1f3e4e718b4a47b5e2bf90
thowell <thowell@stanford.edu>
2021-01-25 21:20:13 -0800

    starting assignment3

--- assignment2/snakes_and_ladders.py
+++ assignment2/snakes_and_ladders.py
@@ -17,8 +17,76 @@ class SnakesAndLaddersState:
     def position(self) -> int:
         return self.position
 
+# special transitions
+def snake_or_ladder(pos):
+    # ladders
+    if pos == 3:
+        return 39
+    elif pos ==  7:
+        return 48
+    elif pos == 12:
+        return 51
+    elif pos == 20:
+        return 41
+    elif pos == 25: 
+        return 57
+    elif pos == 28:
+        return 35
+    elif pos == 45:
+        return 74
+    elif pos == 60:
+        return 85
+    elif pos == 67:
+        return 90
+    elif pos == 69:
+        return 92
+    elif pos == 77:
+        return 83
 
+    # snakes
+    elif pos == 31:
+        return 6
+    elif pos == 38:
+        return 1
+    elif pos == 49:
+        return 8
+    elif pos == 53:
+        return 17
+    elif pos == 65:
+        return 14
+    elif pos == 70:
+        return 34
+    elif pos == 76:
+        return 37
+    elif pos == 82:
+        return 63
+    elif pos == 88:
+        return 50
+    elif pos == 94:
+        return 42
+    elif pos == 98:
+        return 54
+    else:
+        return np.minimum(100, pos)
 
+def end_game_probs(current, next):
+    if current == 95:
+        if next == 100:
+            return 2.0 / 6.0
+    if current == 96: 
+        if next == 100:
+            return 3.0 / 6.0
+    if current == 97:
+        if next == 100:
+            return 4.0 / 6.0
+    if current == 98:
+        if next == 100:
+            return 5.0 / 6.0
+    if current == 99:
+        if next == 100:
+            return 1.0
+    
+    return 1.0 / 6.0
 
 class SnakesAndLaddersMPFinite(FiniteMarkovProcess[SnakesAndLaddersState]):
 
@@ -73,7 +141,7 @@ if __name__ == '__main__':
 
     # print("Stationary Distribution")
     # print("-----------------------")
-    # si_mp.display_stationary_distribution()
+    si_mp.display_stationary_distribution()
 
     # traces
     T = 100
@@ -90,10 +158,10 @@ if __name__ == '__main__':
                 j = T
             else:
                 j += 1
-    # print(len_game)
+    print(len_game)
 
     plt.hist(len_game, density=False, bins=10)  # density=False would make counts
     plt.ylabel('count')
     plt.xlabel('length of game')
     plt.show()
-    # plot.savefig('snakes_and_ladders_length_histogram.png')
+    plot.savefig('snakes_and_ladders_length_histogram.png')
