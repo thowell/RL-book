778c4bae8d0fcd8b4e7ed3ae147b32b92713f6a3
thowell <thowell@stanford.edu>
2021-02-01 21:57:47 -0800

    assignment 4 started

---
+++ assignment4/q1.jl
@@ -0,0 +1,55 @@
+using LinearAlgebra
+
+# states
+S = [:s1, :s2, :s3]
+T = [:s3]
+N = setdiff(S, T)
+
+# actions
+A = [:a1, :a2]
+
+# state transition probability function
+P = Dict()
+
+push!(P, (:s1, :a1, :s1) => 0.2)
+push!(P, (:s1, :a1, :s2) => 0.6)
+push!(P, (:s1, :a1, :s3) => 0.2)
+
+push!(P, (:s1, :a2, :s1) => 0.1)
+push!(P, (:s1, :a2, :s2) => 0.2)
+push!(P, (:s1, :a2, :s3) => 0.7)
+
+push!(P, (:s2, :a1, :s1) => 0.3)
+push!(P, (:s2, :a1, :s2) => 0.3)
+push!(P, (:s2, :a1, :s3) => 0.4)
+
+push!(P, (:s2, :a2, :s1) => 0.5)
+push!(P, (:s2, :a2, :s2) => 0.3)
+push!(P, (:s2, :a2, :s3) => 0.2)
+
+# reward function
+R = Dict()
+
+push!(R, (:s1, :a1) => 8.0)
+push!(R, (:s1, :a2) => 10.0)
+
+push!(R, (:s2, :a1) => 1.0)
+push!(R, (:s2, :a2) => -1.0)
+
+# discount factor 
+γ = 1.0
+
+# value iteration
+V = Dict()
+
+for s in S
+    if s in N
+        push!(V, s => maximum([R[s, a] for a in A]))
+    elseif s in T
+        push!(V, s => 0.0)
+    else
+        @error "value function initialization error"
+    end
+end
+
+V
